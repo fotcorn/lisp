c676ca6651065df16fdb5af686613cb7d8ed8b31
fotcorn <fotcorn@gmail.com>
2016-01-03 03:17:59 +0100

    [Parser] added function call support

--- parser.py
+++ parser.py
@@ -22,23 +22,20 @@ class Identifier(ValueNode):
 
 
 class Function(object):
-    def __init__(self, param_names, expressions):
+    def __init__(self, param_names, expression):
         self.param_names = param_names
-        self.expressions = expressions
+        self.expression = expression
 
     def __str__(self):
-        return u'Function: [{}] ({})'.format(
-                u', '.join(map(unicode, self.param_names)),
-                u', '.join(map(unicode, self.expressions))
-        )
+        return u'Function: [{}] ({})'.format(u', '.join(map(unicode, self.param_names)), self.expression)
 
 
-class Expression(object):
+class Call(object):
     def __init__(self, expressions):
         self.expressions = expressions
 
     def __str__(self):
-        return u'Expression: ({})'.format(u', '.join(map(unicode, self.expressions)))
+        return u'Call: ({})'.format(u', '.join(map(unicode, self.expressions)))
 
 
 def parse(tokens):
@@ -81,18 +78,51 @@ class Parser(object):
             raise Exception(u'Parse error on token {}'.format(self.current_token))
 
     def expression(self):
+        if self.current_token.type == Token.IDENTIFIER and self.current_token.value == 'fn':
+            return self.function()
+        else:
+            return self.call()
+
+    def value(self):
+        if self.accept(Token.NUMBER):
+            return Number(self.last_token.value)
+        elif self.accept(Token.STRING):
+            return String(self.last_token.value)
+        elif self.accept(Token.IDENTIFIER):
+            return Identifier(self.last_token.value)
+        elif self.accept(Token.EXPRESSION_START):
+            return self.expression()
+        elif self.accept(Token.EXPRESSION_END):
+            return None
+        else:
+            raise Exception(u'Parse error on token {}'.format(self.current_token))
+
+    def call(self):
         current_expression = []
         while True:
-            if self.accept(Token.NUMBER):
-                current_expression.append(Number(self.last_token.value))
-            elif self.accept(Token.STRING):
-                current_expression.append(String(self.last_token.value))
-            elif self.accept(Token.IDENTIFIER):
-                current_expression.append(Identifier(self.last_token.value))
-            elif self.accept(Token.EXPRESSION_START):
-                current_expression.append(self.expression())
-            elif self.accept(Token.EXPRESSION_END):
+            value = self.value()
+            if value is None:
+                break
+            current_expression.append(value)
+        return Call(current_expression)
+
+    def function(self):
+        self.next_token()
+        self.expect(Token.BRACKET_OPEN)
+        function_params = []
+        while True:
+            if self.accept(Token.IDENTIFIER):
+                function_params.append(self.last_token.value)
+                if self.accept(Token.COMMA):
+                    continue
+                elif self.accept(Token.BRACKET_CLOSE):
+                    break
+            elif self.accept(Token.BRACKET_CLOSE):
                 break
             else:
-                raise Exception(u'Parse error on token {}'.format(self.current_token))
-        return Expression(current_expression)
+                raise Exception(u'Parse error in function parameter list on token {}'.format(self.current_token))
+        value = self.value()
+        if not value:
+            raise Exception(u'Parse error on token {}'.format(self.current_token))
+        self.expect(Token.EXPRESSION_END)
+        return Function(function_params, value)
