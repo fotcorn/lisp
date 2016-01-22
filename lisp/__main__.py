b34d8da367fc2ac2edb06cae84ffceadc7db9332
fotcorn <fotcorn@gmail.com>
2016-01-22 15:06:28 +0100

    [Core] make interpreter executable as module

--- lisp/__main__.py
+++ lisp/__main__.py
@@ -1,11 +1,22 @@
+import os
+import sys
 import codecs
 
-from lexer import lex
-from parser import parse
-from interpreter import run
+from lisp.lexer import lex
+from lisp.parser import parse
+from lisp.interpreter import run
 
 if __name__ == '__main__':
-    with codecs.open('example.lisp', encoding='utf-8') as f:
+    if len(sys.argv) != 2:
+        print('Usage: python -m lisp <file.lisp>')
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print('Source file {} does not exist'.format(filename))
+        sys.exit(1)
+
+    with codecs.open(filename, encoding='utf-8') as f:
         program = f.read().strip()
     tokens = lex(program)
 
