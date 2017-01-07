20fe4014321bc028480b0e6c88f7a5388bec2e56
fotcorn <fotcorn@gmail.com>
2017-01-07 01:44:02 +0100

    repl: added readline history support

--- lisp/repl.py
+++ lisp/repl.py
@@ -1,6 +1,7 @@
+import os
 import sys
-
 import readline
+import atexit
 
 from lisp import interpreter_builtins
 from lisp.interpreter_exceptions import InterpreterException
@@ -11,6 +12,14 @@ from lisp.interpreter import run, Interpreter
 
 def repl():
     print("Write 'exit' to exit repl")
+
+    history_file = os.path.join(os.path.expanduser("~"), ".fotcorn_lisp_history")
+    try:
+        readline.read_history_file(history_file)
+        readline.set_history_length(1000)
+    except FileNotFoundError:
+        pass
+    atexit.register(readline.write_history_file, history_file)
 
     readline.parse_and_bind('')
 
